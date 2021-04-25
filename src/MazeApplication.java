430648cfc3779147c7f94635f8a2c35c25d4825d
x74717zz <zhaojian.zhen@student.manchester.ac.uk>
2021-04-25 19:14:15 +0100

    Structure and Javadoc of MazeDriver and MazeApplication implemented.

--- src/MazeApplication.java
+++ src/MazeApplication.java
@@ -1,5 +1,21 @@
 import javafx.application.Application;
-
+/** The MazeApplication class is a JavaFX application.
+ *  Allows the user to specify a text file containing a maze representation.
+ *  The graphical representation of the maze is displayed to the user.
+ *  The user will be able to:
+ *  (i) Step through the solving process with the visualisation updating to the current state.
+ *  (ii) Save the current route-solving state to a file.
+ *  (iii) Load route-solving state froma file.
+ *  @author Sam Zhen
+ *  @version 25th April 2021
+ */
 public class MazeApplication extends Application {
 
+	public static void main(String[] args) {
+
+	}
+
+	public static void start(Stage stage) {
+
+	}
 }
--- src/MazeDriver.java
+++ src/MazeDriver.java
@@ -1,4 +1,10 @@
+/** MazeDriver that can print Maze and RouteFinder state to the console/terminal
+ *  @author Sam Zhen
+ *  @version 25th April 2021
+ */
 public class MazeDriver {
+
     public static void main(String args[]) {
+
     }
 }
