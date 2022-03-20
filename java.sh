1b1e89bd9c88472cea28e4c708295172fb9785a1
x74717zz <zhaojian.zhen@student.manchester.ac.uk>
2022-03-20 08:44:16 +0000

    Clean up

--- java.sh
+++
@@ -1,1 +0,0 @@
-java --class-path ./bin --module-path ./lib/ --add-modules=javafx.controls,javafx.fxml $1
--- javac.sh
+++
@@ -1,3 +0,0 @@
-find . -name "*.class" -type f -delete
-
-javac -d ./bin --module-path ./lib/ --add-modules=javafx.controls,javafx.fxml --source-path ./src  $1
